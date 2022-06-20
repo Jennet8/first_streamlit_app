011b05203b030b02c74e26775230777150597b21
Sam <93707300+Jennet8@users.noreply.github.com>
2022-06-20 15:55:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,7 @@ streamlit.header('Header')
 streamlit.text('Body Line 1')
 streamlit.text('Body Line 2')
 
-streamlit.multiselect("Pick some fruits", list(my_fruit_list.index), ['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
