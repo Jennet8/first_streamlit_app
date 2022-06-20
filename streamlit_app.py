3071a1f3be005db7150b95967c27670657d8c1aa
Sam <93707300+Jennet8@users.noreply.github.com>
2022-06-20 15:37:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Cringe')
 
 streamlit.header('Header')
 streamlit.text('Body Line 1')
 streamlit.text('Body Line 2')
+
+streamlit.dataframe(my_fruit_list)
