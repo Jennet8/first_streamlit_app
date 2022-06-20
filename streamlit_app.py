fe0aca2ba350d2d7ef4f1611a140ea6b6d3a7961
Sam <93707300+Jennet8@users.noreply.github.com>
2022-06-20 15:51:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('Cringe')
 
@@ -8,4 +9,5 @@ streamlit.header('Header')
 streamlit.text('Body Line 1')
 streamlit.text('Body Line 2')
 
+streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
